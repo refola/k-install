7a0df792a4b650cd24c5bcd5a25e4ffbc146b938
Mark Haferkamp <refola@users.noreply.github.com>
2018-04-23 06:47:06 -0700

    continue if repo cloned, minor tweaks

--- script/install-k.sh
+++ script/install-k.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-# Debian-like dependencies: Copied from K's README.md at https://github.com/kframework/k5/blob/master/README.md and untested.
+# Debian-like dependencies from K's README.md at https://github.com/kframework/k5/blob/master/README.md (untested).
 deps_deb=(build-essential m4 openjdk-8-jdk libgmp-dev libmpfr-dev pkg-config flex z3 libz3-dev maven opam)
 
-# Chakra-like (Arch-like-like) dependencies: Translated from Debian-like deps and tested by this script's author.
+# Chakra-like dependencies: Translated from Debian-like deps and tested by this script's author.
 deps_chakra=(base-devel m4 openjdk gmp mpfr pkg-config flex maven opam)
 
 # Install all the dependencies for a given distro before building and installing K.
 install_deps() {
     case "$1" in
-        Debian)
+        Debian|*buntu)
             sudo apt-get install "${deps_deb[@]}" || exit 1
                 ;;
-        Chakra)
+        Arch|Chakra)
             sudo pacman --needed -S "${deps_chakra[@]}" || exit 1
-            install_z3_manually || exit 1
+            install_aur_z3-bin || exit 1
             ;;
         *)
             echo "Distro not supported!"
@@ -24,8 +24,8 @@ install_deps() {
 }
 
 # Manually download, build, and install Z3 from https://github.com/Z3Prover/z3
-install_z3_manually() {
-    if pacman -Qi z3-bin; then
+install_aur_z3-bin() {
+    if pacman -Qi z3-bin >/dev/null; then
         return 0
     fi
     echo "Manually installing from Arch User Repository's 'z3-bin' package."
@@ -38,31 +38,39 @@ install_z3_manually() {
     rm -rf "$tmp" || exit 1
 }
 
-install() {
+# Install the K Framework, version 5
+install_k() {
     echo "Installing dependencies."
-    install_deps "$DISTRO" || exit 1
+    install_deps "$1" || exit 1
     echo "Cloning K 5."
     cd "$(dirname "$(readlink -f "$0")")" || exit 1
-    git clone https://github.com/kframework/k5.git || exit 1
+    if [ -d ./k5 ]; then
+        cd k5 || exit 1
+        git pull origin master || exit 1
+        cd - || exit 1
+    else
+        git clone https://github.com/kframework/k5.git || exit 1
+    fi
     echo "Building K."
     cd k5 || exit 1
     export maven_opts="-xx:+tieredcompilation" || exit 1
     mvn package || exit 1
     echo "Setting up OCAML backend."
-    ./k-distribution/target/release/k/bin/k-configure-opam; eval `opam config env` || exit 1
+    ./k-distribution/target/release/k/bin/k-configure-opam; eval "$(opam config env)" || exit 1
     echo "Running fast tests."
     mvn verify -DskipKTest || exit 1
     echo "Building release."
     mvn install || exit 1
 }
 
+# Check parameter existence and (run installation xor show usage message)
 main() {
-    if [ -z "$1" ]; then
+    if [ "$#" != "1" ]; then
         echo "Please specify a distro, either 'Chakra' or 'Debian'." >&2
         exit 1
     fi
-    DISTRO="$1"
-    install || exit 1
+    install_k "$1" || exit 1
 }
 
+# Run it
 main "$@" || exit 1
